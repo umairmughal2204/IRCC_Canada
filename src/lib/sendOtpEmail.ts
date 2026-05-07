db5eccdcf5c29f3a76964ced98a70b09e84366ea
akmin <aftababdullah25@gmail.com>
2026-05-07 23:31:43 +0500

    Updated

--- src/lib/sendOtpEmail.ts
+++ src/lib/sendOtpEmail.ts
@@ -11,7 +11,7 @@ export async function sendOtpEmail(to: string, otp: string) {
   });
 
   const mailOptions = {
-    from: `"Government of Canada" <GC@auth.cananda.ca>`,
+    from: `"Government of Canada"`,
     to,
     subject: "Verify your login - Government of Canada",
     html: `
